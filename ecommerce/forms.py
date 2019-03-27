fc86156b098c967a6037516c90fb7a95d1ac39cc
Vincent Moyaki <Moyakivincent@yahoo.com>
2019-03-27 18:27:27 +0100

    add contact form

---
+++ ecommerce/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+    fullname = forms.CharField(widget=forms.TextInput(
+        attrs={
+            "class": "form-control",
+            "placeholder": "Your fullname"
+        }))
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={
+            "class": "form-control",
+            "placeholder": "Your email"
+        }))
+    content = forms.CharField(widget=forms.Textarea(
+        attrs={
+            "class": "form-control",
+                     "placeholder": "Your Text"
+        }))
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not "gmail.com" in email:
+            raise forms.ValidationError("Enter a valid email")
+        return email
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from ecommerce.forms import ContactForm
+
 
 def home_page(request):
     context = {
@@ -16,7 +18,15 @@ def about_page(request):
 
 
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
     context = {
-        "title": "Contact Us"
+        "title": "Contact Us",
+        "form": contact_form
     }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    # if request.method == "POST":
+    #     print(request.POST.get('fullname'))
+    #     print(request.POST.get('email'))
+    #     print(request.POST.get('content'))
     return render(request, "contact_page.html", context)
