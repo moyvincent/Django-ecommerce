96f229bb95fbcfab5a65926931dc40c32590d2a3
Vincent Moyaki <Moyakivincent@yahoo.com>
2019-06-07 19:57:26 +0100

    add and remove from cart

--- carts/views.py
+++ carts/views.py
@@ -5,16 +5,22 @@ from carts.models import Cart
 
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
-    return render(request, 'carts/home.html', {})
+    return render(request, 'carts/home.html', {"cart": cart_obj})
 
 def cart_update(request):
-    product_id = 1
-    product_obj = Product.objects.get(id=product_id)
-    cart_obj, new_obj = Cart.objects.new_or_get(request)
-    if product_obj in cart_obj.product.all():
-        cart_obj.product.remove(product_obj)
-    else:
-        cart_obj.product.add(product_obj)
+    product_id = request.POST.get('product_id')
+    if product_id is not None:
+        try:
+            product_obj = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            print('show something')
+            return redirect("cart:home")
+        cart_obj, new_obj = Cart.objects.new_or_get(request)
+        if product_obj in cart_obj.product.all():
+            cart_obj.product.remove(product_obj)
+        else:
+            cart_obj.product.add(product_obj)
+        request.session['cart_item'] = cart_obj.product.count()
     return redirect("cart:home")
 
 
